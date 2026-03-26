a0912b71c045792b269bd52e615799081f565564
hajun0944 <dj20250603@dge.go.kr>
2026-03-26 10:39:39 +0900

    Add files via upload

---
+++ jun.java
@@ -0,0 +1,13 @@
+package jump2java;
+
+public class jun {
+	public static void main(String[] args) {
+		boolean hascard = true;
+		int money1 = 2000;
+		if (money1 >= 3000) {
+			System.out.println("택시를 타고 가라");
+		} else {
+			System.out.println("걸어가라");
+		}
+	}
+}
