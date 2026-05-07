39b79254fc4350248c3bdab1258765f0a33f38f6
hajun0944 <dj20250603@dge.go.kr>
2026-05-07 10:16:33 +0900

    Add files via upload

---
+++ PrimeNumber.java
@@ -0,0 +1,36 @@
+package jump2java;
+import java.util.Scanner;
+		
+public class PrimeNumber {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num;
+		
+		while(true) {
+			System.out.println("2이상 정수 입력해");
+			num = sc.nextInt();
+			
+			if(num>=2) {
+				break;
+			}
+		}
+		
+		boolean isPrime = true;
+		
+		for(int i=2; i<num; i++ ) {
+			if(num % i == 0) {
+				isPrime = false;
+				break;
+			}
+		}
+		
+		if (isPrime) {
+			System.out.println("소수이다");
+		}else {
+			System.out.println("소수 아니다");
+		}	
+		sc.close();
+	}	
+		
+}		
+		
